ca89ada4eab7df97a17959c1a6bf1a3e51cf2af4
eugenioclrc <eugenioclrc@gmail.com>
2023-02-11 00:00:39 -0300

    add text to home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,8 @@
 		<div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
 			<div class="py-12">
 				<h1 class=" font-semibold text-5xl">Huff Binary Function Dispatching</h1>
-				<p class="mt-4 text-lg text-gray-700">This is a simple example of a Landing Page you can build using Tailwind
-					Starter Kit. It features multiple CSS components based on the Tailwindcss design system.<br />
+				<p class="mt-4 text-lg text-gray-700">This is a demo that tries to create a simple 
+					template using a binary search dispatcher based on a given ABI json interface.<br />
 				<a href="https://docs.huff.sh/tutorial/function-dispatching/#binary-search-dispatching" 
 					target="_blank" rel="noreferrer" class="underline text-blue-700 hover:font-bold">Sorce article</a> - 
 					<a href="https://github.com/eugenioclrc/huff-Binary-Search-Dispatching" 
